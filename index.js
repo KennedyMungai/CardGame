ba59e3ed597b56df3be6083bc6460aba5f75d27c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-17 10:13:58 +0300

    Defined the createCard and createELement functions

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const cardObjectDefinitions = [
     { id: 3, imagePath: "/images/card-QueenDiamonds.png" },
     { id: 4, imagePath: "/images/card-AceSpades.png" },
 ]
+
+function createCard(cardItem)
+{
+    const cardElem = document.createElement('div')
+}
+
+function createElement(elemType)
+{
+    return document.createElement(elemType)
+}
